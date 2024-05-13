10b3166c07cefa170a9548ea757ea3c75a2e6ae1
Mukta9904 <142688686+Mukta9904@users.noreply.github.com>
2024-05-13 10:37:58 +0530

    fixed

--- src/app/api/user/profile/route.js
+++ src/app/api/user/profile/route.js
@@ -9,7 +9,7 @@ connection()
 export async function GET(request){
  try {
     //get data from token
-   const userId = getTokenData(request)
+   const userId = await getTokenData(request)
    const user = await User.findOne({_id: userId}).select("-password")
    if(!user){
     return NextResponse.json({message: "User not found"},{status:400})
